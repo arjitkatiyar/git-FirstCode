efd8cd27f46dd6ed045e3cc4f89a58c094b23f5d
Arjit katiyar <arjitkatiyar395@gmail.com>
2024-04-27 22:59:18 +0530

    committed

---
+++ Recursion1.java
@@ -0,0 +1,17 @@
+
+
+class Recursion1 {
+    public static void RecusionDemo(int n){
+        if (n==0){
+            return;
+        }
+        System.out.println(n);
+        RecusionDemo(n-1);
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        RecusionDemo(n);
+    }
+    
+}
